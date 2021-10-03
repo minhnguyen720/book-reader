0005e5e7a3f25f8c047556d3612360e1112af7b3
Truong Duong Le <53457340+duongross@users.noreply.github.com>
2021-10-03 09:26:41 +0700

    add chapterposition

--- DB/storeprocedure/getChapter.sql
+++ DB/storeprocedure/getChapter.sql
@@ -1,6 +1,6 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `getChapter`(book_name VARCHAR(50))
 BEGIN    
-    SELECT ChapterID, ChapterName
+    SELECT ChapterID, ChapterName, ChapterPosition
     FROM book
 		NATURAL JOIN chapter
     WHERE BookName LIKE book_name;
