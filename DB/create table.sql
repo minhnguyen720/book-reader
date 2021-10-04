ba39a592321f50aa744570691c10e4dc68d5e4b1
Truong Duong Le <53457340+duongross@users.noreply.github.com>
2021-10-04 21:35:17 +0700

    fix

--- DB/create table.sql
+++ DB/create table.sql
@@ -6,12 +6,12 @@ CREATE TABLE book (
 );
 
 CREATE TABLE chapter (
-	ChapterID			VARCHAR(6),
-    BookID				VARCHAR(6),
-    ChapterName			VARCHAR(50),
-    ChapterPosition		VARCHAR(100),
+	ChapterID	VARCHAR(6),
+    BookID		VARCHAR(6),
+    ChapterName	VARCHAR(50),
+    Content		MEDIUMTEXT,
     
-    primary KEY (ChapterID, BookID),
+    PRIMARY KEY (ChapterID, BookID),
     foreign key (BookID) references book(BookID)
 )
 
@@ -21,20 +21,20 @@ INSERT INTO `project`.`book` (`BookID`, `BookName`) VALUES ('3', 'The Dog Soul')
 INSERT INTO `project`.`book` (`BookID`, `BookName`) VALUES ('4', 'The Cat Soul');
 INSERT INTO `project`.`book` (`BookID`, `BookName`) VALUES ('5', 'The Bird Soul');
 
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('1', '1', 'Introduction', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('2', '1', 'Zwei', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('3', '1', 'Drei', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('4', '1', 'Fear', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('5', '1', 'Fume', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('1', '2', 'Introduction', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('2', '2', 'Zwei', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('3', '2', 'Drei', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('1', '3', 'Introduction', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('2', '3', 'Zwei', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('3', '3', 'Drei', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('4', '3', 'Fear', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('1', '4', 'Introduction', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('1', '5', 'Introduction', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('2', '5', 'Zwei', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('3', '5', 'Drei', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('4', '5', 'Fear', 'C:UsersPCDownloadsCompressed');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `Content`) VALUES ('1', '1', 'Introduction', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `Content`) VALUES ('2', '1', 'Zwei', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `Content`) VALUES ('3', '1', 'Drei', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `Content`) VALUES ('4', '1', 'Fear', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `Content`) VALUES ('5', '1', 'Fume', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `Content`) VALUES ('1', '2', 'Introduction', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `Content`) VALUES ('2', '2', 'Zwei', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `Content`) VALUES ('3', '2', 'Drei', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `Content`) VALUES ('1', '3', 'Introduction', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `Content`) VALUES ('2', '3', 'Zwei', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `Content`) VALUES ('3', '3', 'Drei', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `Content`) VALUES ('4', '3', 'Fear', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `Content`) VALUES ('1', '4', 'Introduction', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `Content`) VALUES ('1', '5', 'Introduction', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `Content`) VALUES ('2', '5', 'Zwei', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `Content`) VALUES ('3', '5', 'Drei', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `Content`) VALUES ('4', '5', 'Fear', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry');
