59e8cb5c2276dcafa038ffbb61d1e0508270725e
Truong Duong Le <53457340+duongross@users.noreply.github.com>
2021-10-04 12:19:46 +0700

    change table

--- DB/create table.sql
+++ DB/create table.sql
@@ -1,40 +1,40 @@
 CREATE TABLE book (
-	BookID	VARCHAR(6),
-    BookName	VARCHAR(50),
+	BookID				VARCHAR(6),
+    BookName			VARCHAR(50),
     
     PRIMARY KEY(BookID)
 );
 
 CREATE TABLE chapter (
-	ChapterID	VARCHAR(6),
-    BookID	VARCHAR(6),
-    ChapterName VARCHAR(50),
-    ChapterPosition VARCHAR(150),
+	ChapterID			VARCHAR(6),
+    BookID				VARCHAR(6),
+    ChapterName			VARCHAR(50),
+    ChapterPosition		VARCHAR(100),
     
-    PRIMARY KEY (ChapterID),
-    FOREIGN KEY (BookID) REFERENCES book(BookID)
-);
+    primary KEY (ChapterID, BookID),
+    foreign key (BookID) references book(BookID)
+)
 
-INSERT INTO `project`.`book` (`BookID`, `BookName`) VALUES ('B00001', 'Book1');
-INSERT INTO `project`.`book` (`BookID`, `BookName`) VALUES ('B00002', 'Book2');
-INSERT INTO `project`.`book` (`BookID`, `BookName`) VALUES ('B00003', 'Book3');
-INSERT INTO `project`.`book` (`BookID`, `BookName`) VALUES ('B00004', 'Book4');
-INSERT INTO `project`.`book` (`BookID`, `BookName`) VALUES ('B00005', 'Book5');
+INSERT INTO `project`.`book` (`BookID`, `BookName`) VALUES ('1', 'The Chicken Soul');
+INSERT INTO `project`.`book` (`BookID`, `BookName`) VALUES ('2', 'The Duck Soul');
+INSERT INTO `project`.`book` (`BookID`, `BookName`) VALUES ('3', 'The Dog Soul');
+INSERT INTO `project`.`book` (`BookID`, `BookName`) VALUES ('4', 'The Cat Soul');
+INSERT INTO `project`.`book` (`BookID`, `BookName`) VALUES ('5', 'The Bird Soul');
 
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('C10001', 'B00001', 'Chap 1', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('C10002', 'B00001', 'Chap 2', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('C10003', 'B00001', 'Chap 3', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('C10004', 'B00001', 'Chap 4', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('C10005', 'B00001', 'Chap 5', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('C20001', 'B00002', 'Chap 1', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('C20002', 'B00002', 'Chap 2', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('C20003', 'B00002', 'Chap 3', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('C30001', 'B00003', 'Chap 1', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('C30002', 'B00003', 'Chap 2', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('C30003', 'B00003', 'Chap 3', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('C30004', 'B00003', 'Chap 4', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('C40001', 'B00004', 'Chap 1', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('C50001', 'B00005', 'Chap 1', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('C50002', 'B00005', 'Chap 2', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('C50003', 'B00005', 'Chap 3', 'C:UsersPCDownloadsCompressed');
-INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('C50004', 'B00005', 'Chap 4', 'C:UsersPCDownloadsCompressed');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('1', '1', 'Introduction', 'C:UsersPCDownloadsCompressed');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('2', '1', 'Zwei', 'C:UsersPCDownloadsCompressed');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('3', '1', 'Drei', 'C:UsersPCDownloadsCompressed');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('4', '1', 'Fear', 'C:UsersPCDownloadsCompressed');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('5', '1', 'Fume', 'C:UsersPCDownloadsCompressed');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('1', '2', 'Introduction', 'C:UsersPCDownloadsCompressed');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('2', '2', 'Zwei', 'C:UsersPCDownloadsCompressed');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('3', '2', 'Drei', 'C:UsersPCDownloadsCompressed');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('1', '3', 'Introduction', 'C:UsersPCDownloadsCompressed');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('2', '3', 'Zwei', 'C:UsersPCDownloadsCompressed');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('3', '3', 'Drei', 'C:UsersPCDownloadsCompressed');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('4', '3', 'Fear', 'C:UsersPCDownloadsCompressed');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('1', '4', 'Introduction', 'C:UsersPCDownloadsCompressed');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('1', '5', 'Introduction', 'C:UsersPCDownloadsCompressed');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('2', '5', 'Zwei', 'C:UsersPCDownloadsCompressed');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('3', '5', 'Drei', 'C:UsersPCDownloadsCompressed');
+INSERT INTO `project`.`chapter` (`ChapterID`, `BookID`, `ChapterName`, `ChapterPosition`) VALUES ('4', '5', 'Fear', 'C:UsersPCDownloadsCompressed');
