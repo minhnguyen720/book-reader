dfff821b19a7ac46247ecdda3cf7a3a376ad1554
Truong Duong Le <53457340+duongross@users.noreply.github.com>
2021-10-04 12:20:45 +0700

    change id into increment

--- DB/storeprocedure/getBook.sql
+++ DB/storeprocedure/getBook.sql
@@ -1,6 +1,6 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `getBook`(book_name VARCHAR(50))
 BEGIN
-	set book_name = concat('%', book_name, '%'); 
+	set book_name = concat(book_name,'%'); 
     if book_name is NULL then 
 		set book_name = '%';
 	end if;
--- DB/storeprocedure/setChapter.sql
+++ DB/storeprocedure/setChapter.sql
@@ -3,24 +3,26 @@ BEGIN
 	DECLARE ChapterID VARCHAR(6);
     DECLARE ChapterPosition VARCHAR(100);
     DECLARE flag INT;
+    DECLARE code INT;
     DECLARE book_name_temp VARCHAR(50);
     DECLARE chapter_name_temp VARCHAR(50);
-    
+	DECLARE chapterlocation VARCHAR(100);
+
     SET book_name_temp=BookName;
 	SET chapter_name_temp=ChapterName;
-    SET ChapterPosition = concat('C:Users/PC/Downloads/Compressed/', REPLACE(book_name_temp,' ',''),REPLACE(chapter_name_temp,' ',''),'.txt');
+    SET ChapterPosition = concat('C:Users/PC/Downloads/Compressed/', REPLACE(book_name_temp,' ',''),"_",REPLACE(chapter_name_temp,' ',''),'.html');
+    SET chapterlocation = ChapterPosition;
     SET flag=0;
-    SET ChapterID = concat('C',floor(rand()*100000));
+    SET code = (SELECT COUNT(C.ChapterID) 
+				from chapter C 
+                    WHERE C.BookID=(SELECT B.BookID FROM book B WHERE B.BookName=BookName));
+    SET ChapterID= CAST((code+1)as char(6));
     
-    WHILE ChapterID IN (SELECT C.ChapterID FROM chapter C)
-		DO
-			SET ChapterID = concat('C',floor(rand()*100000));
-		END WHILE;
     IF replace(book_name_temp,' ','') !='' AND replace(chapter_name_temp,' ','') !=''
 	THEN IF BookName IN (SELECT B.BookName FROM book B) 
 		THEN INSERT INTO chapter VALUES (ChapterID,(SELECT B.BookID FROM book B WHERE B.BookName=BookName),ChapterName,ChapterPosition);
         SET flag=1;
 		END IF;
 	END IF;
-    SELECT flag;
+    SELECT flag, chapterlocation;
 END
