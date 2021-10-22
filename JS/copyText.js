f7ee2d541a6e09948326f1a76b4bb22794288d96
Truong Duong Le <53457340+duongross@users.noreply.github.com>
2021-10-22 21:08:33 +0700

    Delete JS directory

--- JS/copyText.js
+++
@@ -1,9 +0,0 @@
-function copyText() {
-    let selObj = window.getSelection();
-    let selText = selObj.toString()
-    if (selText.length > 0) {
-        navigator.clipboard.writeText(selText);
-        alert("Copied: " + selText);
-        
-    } else alert("Please select somthing")
-}
