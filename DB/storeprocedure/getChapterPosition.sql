93fff66b43f352a170800bd5f732a3fd9576e472
Truong Duong Le <53457340+duongross@users.noreply.github.com>
2021-10-22 19:24:37 +0700

    Delete getChapterPosition.sql

--- DB/storeprocedure/getChapterPosition.sql
+++
@@ -1,6 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `getChapterPosition`(chapter_id VARCHAR(6))
-BEGIN
-    SELECT ChapterPosition
-    FROM chapter
-    WHERE ChapterID LIKE chapter_id;
-END
